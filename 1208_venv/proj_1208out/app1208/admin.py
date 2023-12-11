7d8093968525aebc1f04347e9c98197fb306c0f1
Jesus G <113757515+py3user22@users.noreply.github.com>
2023-12-10 23:34:50 -0600

    save1 for 1208
    working app but missing the reservations model to work, fixed in 2nd demo,

---
+++ 1208_venv/proj_1208out/app1208/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Menu
+from .models import Booking
+# from .models import Menu, Booking
+
+
+admin.site.register(Menu)
+admin.site.register(Booking)
---
+++ 1208_venv/proj_1208out/app1208/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class App1208Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app1208'
